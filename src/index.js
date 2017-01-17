b1ef804f9aca19a52fce3baf03bda51783268d38
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-01-17 21:15:21 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 // @flow
-const path = require('path')
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const https = require('https');
+const path = require('path'),
+    express = require('express'),
+    config = require('config'),
+    crypto = require('crypto'),
+    app = express(),
+    fs = require('fs'),
+    https = require('https'),
+    bodyParser = require('body-parser');
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
 
 
-
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req ,res) {
+app.get('/', function (req, res) {
     res.send('Hi, my name s Ganf')
 });
 app.get('/webhook', function (req, res) {
+    // verify facebook msg api
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === "raise-of-my-butt-hole") {
+        req.query['hub.verify_token'] === "") {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']);
     } else {
@@ -23,6 +48,38 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.post('/webhook', function (req, res) {
+    const data = req.body;
+
 });
+
+
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
